9b2e6a87b51134bd7d313333c366ba5b7ad3434d
Sam Mason <sam.jbmason@gmail.com>
2013-05-14 09:37:59 +0100

    Added latency compensation to filter

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -1,3 +1,16 @@
 Meteor.Router.add({
   '/': 'messagesList'
 });
+
+Meteor.Router.filters({
+  'requireLogin': function(page) {
+    if (Meteor.user())
+      return page;
+    else if (Meteor.loggingIn())
+      return 'loading';
+    else
+      return 'loggedOut';
+  }
+});
+
+Meteor.Router.filter('requireLogin', {only: 'messagesList'});

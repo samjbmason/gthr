2bb2eab067dd6a9a6b60b6f4cf91ab834f972b23
Sam Mason <sam.jbmason@gmail.com>
2013-05-14 19:20:59 +0100

    Added comment to account block file

--- server/account-block.js
+++ server/account-block.js
@@ -1,3 +1,7 @@
+/*--------- 
+Not perfect, but for the time being I will be using manual methods to block account creation until they allow new fields to be added to signup box and I can create a security question
+---------*/
+
 //Accounts.config({forbidClientAccountCreation: true});
 Accounts.validateNewUser(function(user) {
    if (Meteor.users.find().count() === 0)

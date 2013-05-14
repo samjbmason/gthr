4bc9da55413c77c84a4bb449f8799c2f5d1a9723
Sam Mason <sam.jbmason@gmail.com>
2013-05-14 23:42:11 +0100

    Added url converter for messages and output works

--- client/views/messages/message_entry.js
+++ client/views/messages/message_entry.js
@@ -2,8 +2,16 @@ Template.messageEntry.events({
 	'submit form': function(event) {
 		event.preventDefault();
 
+		function replaceWithLinks(text) {
+    	var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    	return text.replace(exp,"<a target='_blank' href='$1'>$1</a>"); 
+		}
+
+		var rawMessage = $(event.target).find('[name=content]').val();
+		var prettyMessage = replaceWithLinks(rawMessage);
+
 		var message = {
-			content: $(event.target).find('[name=content]').val()
+			content: prettyMessage
 		}
 
 		Meteor.call('postMessage',message,function(error,id) {
@@ -15,4 +23,6 @@ Template.messageEntry.events({
 		}); 
 
 	}
+
+	
 });

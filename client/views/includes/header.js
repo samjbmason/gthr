de940c2a7acee59c63779183b08313071a02bfaf
Sam Mason <sam.jbmason@gmail.com>
2013-05-14 11:53:11 +0100

    Tidied up

--- client/views/includes/header.js
+++ client/views/includes/header.js
@@ -7,20 +7,20 @@ Template.header.helpers({
 Template.header.created = function() {
 	navigator.geolocation.getCurrentPosition(success_callback,error_callback);
 		
-		function success_callback(p){
-			// Building Latitude = 51.522206
-			// Building Longitude = -0.078305
-			var lat = parseFloat(p.coords.latitude);
-			var lon = parseFloat(p.coords.longitude);
-		  
-		  if( lat >= 51.521606 && lat <= 51.522606 && lon >= -0.078805  && lon <=  -0.077705 ) {
-		  	Session.set("locationCheck",1);
-		  } else {
-		  	Session.set("locationCheck",0);
-		  }
-		}
+	function success_callback(p){
+		// Building Latitude = 51.522206
+		// Building Longitude = -0.078305
+		var lat = parseFloat(p.coords.latitude);
+		var lon = parseFloat(p.coords.longitude);
+	  
+	  if( lat >= 51.521606 && lat <= 51.522606 && lon >= -0.078805  && lon <=  -0.077705 ) {
+	  	Session.set("locationCheck",1);
+	  } else {
+	  	Session.set("locationCheck",0);
+	  }
+	}
 
-		function error_callback(p){
-		     Session.set("locationCheck",0);
-		}
-  }
+	function error_callback(p){
+	     Session.set("locationCheck",0);
+	}
+}

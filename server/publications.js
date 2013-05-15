9ec9683679675b6c62a17ef77d87550d2b91ac8e
Sam Mason <sam.jbmason@gmail.com>
2013-05-15 10:14:18 +0100

    Subscribed and publicised to presence

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,7 @@
 Meteor.publish('messages', function(limit) {
 	return Messages.find({}, {sort: {submitted: -1}, limit: limit});
 });
+
+Meteor.publish('userPresence', function() {
+  return Meteor.presences.find({}, {fields: {state: true, userId: true}});
+});

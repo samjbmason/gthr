fd6ef6847b7126c6d42d50645968117b817716ec
Sam Mason <sam.jbmason@gmail.com>
2013-05-16 09:11:25 +0100

    Add animation

--- client/views/messages/message_single.js
+++ client/views/messages/message_single.js
@@ -5,3 +5,19 @@ Template.messageSingle.helpers({
 		return niceDate;
 	}
 });
+
+Template.messageSingle.rendered = function() {
+	var instance = this;
+	var $this = $(this.firstNode);
+
+	if (typeof(instance.currentPosition) !== 'undefined') {
+		$this.addClass("oldPost");
+	} else {
+		$this.addClass("invisible");
+	}
+
+	Meteor.defer(function() {
+		instance.currentPosition = 1;
+    $this.removeClass("invisible");
+  }); 
+};

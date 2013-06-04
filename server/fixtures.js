8e8ccf825d08dc62cf2eb7064ba41403210a6001
Sam Mason <sam.jbmason@gmail.com>
2013-06-04 09:56:55 +0100

    stopped adding fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,41 +1,41 @@
-if (Messages.find().count() === 0) {
+if (Messages.find().count() === 2) {
   var now = new Date().getTime();
 
   var tomId = Meteor.users.insert({
-    profile: { name: 'Tom Coleman' }
+    profile: { name: 'Tom Coleman', username: 'tomcoleman' }
   });
   var tom = Meteor.users.findOne(tomId);
 
   var sachaId = Meteor.users.insert({
-    profile: { name: 'Sacha Greif' }
+    profile: { name: 'Sacha Greif', username: 'sachagreif' }
   });
   var sacha = Meteor.users.findOne(sachaId);
   
   Messages.insert({
     content: 'Hi everyone',
     userId: sacha._id,
-    name: sacha.profile.name,
+    name: sacha.profile.username,
     submitted: now - 5 * 3600 * 1000
   });
 
   Messages.insert({
     content: 'Hi Sacha',
     userId: tom._id,
-    name: tom.profile.name,
+    name: tom.profile.username,
     submitted: now - 7 * 3600 * 1000
   });
 
   Messages.insert({
     content: 'Wow this is cool',
     userId: tom._id,
-    name: tom.profile.name,
+    name: tom.profile.username,
     submitted: now - 8 * 3600 * 1000,
   });
 
   for (var i = 0; i < 10; i++) {
     Messages.insert({
       content: 'Test message #' + i,
-      name: sacha.profile.name,
+      name: sacha.profile.username,
       userId: sacha._id,
       submitted: now - i * 3600 * 1000
     });

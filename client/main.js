334ebed2f0270e6977b51398186f253abc3d7745
Sam Mason <sam.jbmason@gmail.com>
2013-06-06 11:48:12 +0100

    Created Global front page helper

--- client/main.js
+++ client/main.js
@@ -5,3 +5,10 @@ Meteor.subscribe('users');
 Meteor.startup(function () {
 		Meteor.subscribe('userPresence'); 
 }); 
+
+// Global Helper for adding front-page class to body tag
+Handlebars.registerHelper("isFrontPage", function() {
+	if(Meteor.Router.page()=='frontPage') {
+  	return 'front-page';
+	}
+});

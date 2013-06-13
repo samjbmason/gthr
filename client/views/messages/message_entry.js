a3144ef9778b5d1fab2f3de65f3f899044271e6a
Sam Mason <sam.jbmason@gmail.com>
2013-06-13 02:06:31 +0100

    Overhaul function may need a bit more to do

--- client/views/messages/message_entry.js
+++ client/views/messages/message_entry.js
@@ -1,39 +1,39 @@
 Template.messageEntry.events({
-	'submit form': function(event) {
-		event.preventDefault();
+	'click input[type="submit"], keypress .entry-field': function(event) {
+		if(event.type=="click"||event.which==13){
+			event.preventDefault();
 
-		function replaceWithLinks(text) {
-    	var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-    	return text.replace(exp,"<a target='_blank' href='$1'>$1</a>"); 
+			function replaceWithLinks(text) {
+	    	var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+	    	return text.replace(exp,"<a target='_blank' href='$1'>$1</a>"); 
+			}
+
+			var rawMessage = $('form').find('[name=content]').val();
+			var prettyMessage = replaceWithLinks(rawMessage);
+
+			var message = {
+				content: prettyMessage
+			}
+
+			Meteor.call('postMessage',message,function(error,id) {
+				if (error) {
+	      	 return throwError(error.reason);
+	      } else {
+	      	$('form').find('[name=content]').val("").focus();
+	      	var mess=Messages.findOne(id);
+	      	var myNotification = webkitNotifications.createNotification('icon.png', 'New message from '+mess.name, mess.content);
+	      	console.log(mess.userId+'---'+Meteor.userId());
+	      	if(mess.userId!==Meteor.userId()){
+						myNotification.show();
+					}
+
+	    	}
+			}); 
 		}
-
-		var rawMessage = $(event.target).find('[name=content]').val();
-		var prettyMessage = replaceWithLinks(rawMessage);
-
-		var message = {
-			content: prettyMessage
-		}
-
-		Meteor.call('postMessage',message,function(error,id) {
-			if (error) {
-      	 return throwError(error.reason);
-      } else {
-      	$(event.target).find('[name=content]').val("").focus();
-    	}
-		}); 
-
 	}
-
 });
 
 Template.messageEntry.rendered = function(){
 	$('.entry-field').autosize({append: "\n"});
-
-	$(".entry-field").keypress(function(event) {
-    if (event.which == 13) {
-        event.preventDefault();
-        $(".submit-btn").click();
-    }
-	});
 };   
 

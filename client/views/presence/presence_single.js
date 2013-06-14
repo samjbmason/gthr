80a791e17a940ff2ec046ce6dd9da47f7dc4312a
Sam Mason <sam.jbmason@gmail.com>
2013-06-14 21:53:54 +0100

    Created avatar helper

--- client/views/presence/presence_single.js
+++ client/views/presence/presence_single.js
@@ -1,5 +1,19 @@
 Template.presenceSingle.helpers({
 	isOnline: function() {
   	return Meteor.presences.findOne({userId: this._id});
+  },
+  isYou: function() {
+  	if(Meteor.userId()===this._id) {
+  		return true;
+  	} else {
+  		return false;
+  	}
+  },
+  avatar: function() {
+  	user= Meteor.user();
+  	email= user.emails[0].address;
+  	emailHash = CryptoJS.MD5(email);
+  	avatarUrl= 'http://www.gravatar.com/avatar/'+emailHash;
+  	return avatarUrl;
   }
 });
